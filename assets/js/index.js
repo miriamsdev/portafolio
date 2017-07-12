fe3a4ce23fa3c250c846fd448c5009cc7921ff85
Miriam Peralta <miiriampc@gmail.com>
2017-07-11 19:08:10 -0500

    carousel portfolio

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,17 +5,18 @@ $(window).scroll(() => {
         $('.header').css('background', '#3ed6bb');
         $('.header__menu a').css('color', '#fff');
         $('.header__logo img').css('width','70px');
-        // $('.header').attr
         $('.header__logo img').attr('src','assets/images/logo-blanco.png');
-        $('.header__menu>li').mouseover(() => {
-            $(this).css('border-bottom','4px solid #fff');
-        });
     } else {
         $('.header__logo img').attr('src','assets/images/logo-miriam.png');
         $('.header').css('background', 'rgba(62, 70, 101, 0.78)');
         $('.header__logo img').css('width','70px');
     }
 });
+
+const state = {
+    project: null
+};
+
 //Function: Acordeon
 $( _ => {
     $(".accordion__title").click(function(e){
@@ -29,4 +30,6 @@ $( _ => {
             content.slideUp(250);
         }
     });
+
+
 });
